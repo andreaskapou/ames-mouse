519ea9a33581c960b81b9ee052cf388644086215
andreaskapou <kapouranis.andreas@gmail.com>
2016-05-04 14:14:00 +0100

    Finalize script for all BS-Seq files

--- src/convert_cov_to_encode.R
+++ src/convert_cov_to_encode.R
@@ -14,25 +14,41 @@ R.utils::sourceDirectory("lib", modifiedOnly = FALSE)
 bs_files  <- c("../datasets/BEATSON/BS-Seq/1L_df_old.CpG_context.aggregate",
                "../datasets/BEATSON/BS-Seq/2L_df_old.CpG_context.aggregate",
                "../datasets/BEATSON/BS-Seq/3L_df_old.CpG_context.aggregate",
-               "../datasets/BEATSON/BS-Seq/4L_df_old.CpG_context.aggregate")
+               "../datasets/BEATSON/BS-Seq/4L_df_old.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/5L_N_old.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/6L_N_old.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/7L_N_old.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/8L_N_old.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/9L_df_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/10L_df_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/11L_df_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/12L_df_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/13L_N_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/14L_N_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/15L_N_young.CpG_context.aggregate",
+               "../datasets/BEATSON/BS-Seq/16L_N_young.CpG_context.aggregate"
+               )
 
-bs_data <- read_bs_bismark_cov(bs_files[1], is_GRanges = FALSE)
+for (i in 1:length(bs_files)){
+  bs_data <- read_bs_bismark_cov(bs_files[i], is_GRanges = FALSE)
 
-filename = paste0(bs_files[1], ".bed")
-data = cbind(chr         = bs_data$chr, 
-             chromStart  = bs_data$start, 
-             chromEnd    = bs_data$start + 1, 
-             name        = rep("N", length(bs_data$chr)), 
-             score       = bs_data$meth_reads + bs_data$unmeth_reads,
-             strand      = rep(".", length(bs_data$chr)),
-             thickStart  = rep(".", length(bs_data$chr)),
-             thickEnd    = rep(".", length(bs_data$chr)), 
-             itemRgb     = rep(".", length(bs_data$chr)), 
-             readCount   = bs_data$meth_reads + bs_data$unmeth_reads,
-             percentMeth = ceiling(100 * (bs_data$meth_reads / (bs_data$meth_reads + bs_data$unmeth_reads)))
-)
-write.table(x = data, 
-            file = filename, 
-            sep = "\t", 
-            quote = FALSE, 
-            row.names = FALSE)
+  filename = paste0(bs_files[i], ".bed")
+  final_data = cbind(chr         = bs_data$chr, 
+                     chromStart  = bs_data$start, 
+                     chromEnd    = bs_data$start + 1, 
+                     name        = rep(1, length(bs_data$chr)), 
+                     score       = bs_data$meth_reads + bs_data$unmeth_reads,
+                     strand      = rep(1, length(bs_data$chr)),
+                     thickStart  = rep(1, length(bs_data$chr)),
+                     thickEnd    = rep(1, length(bs_data$chr)), 
+                     itemRgb     = rep(1, length(bs_data$chr)), 
+                     readCount   = bs_data$meth_reads + bs_data$unmeth_reads,
+                     percentMeth = ceiling(100 * (bs_data$meth_reads / (bs_data$meth_reads + bs_data$unmeth_reads)))
+  )
+  write.table(x = final_data, 
+              file = filename, 
+              sep = "\t", 
+              quote = FALSE, 
+              row.names = FALSE)
+}
+rm(final_data)
