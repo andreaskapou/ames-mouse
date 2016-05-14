5094616070d0109f3cf8f49260b9b2a540cddbbe
andreaskapou <kapouranis.andreas@gmail.com>
2016-05-14 12:29:11 +0100

    Minor changes

--- src/m3d_statistic.R
+++ src/m3d_statistic.R
@@ -22,7 +22,7 @@ bs_files  <- c("../datasets/BEATSON/BS-Seq/processed/1L_df_old.pool.bed",
 rna_files  <- "../datasets/BEATSON/RNA-Seq/processed/RNA_Seq_FPKM_X1L_df_old.bed"
 
 ##----------------- Initialize -----------------------
-downstream <- 7000
+downstream <- 5000
 # Read BS data
 bs_data  <- preprocess_bs_seq(files = bs_files[1],
                               file_format = "encode_rrbs")
@@ -40,46 +40,46 @@ methyl_region <- create_methyl_region(bs_data       = bs_data,
 prom_regions <- prom_regions[methyl_region$prom_ind]
 
 
-##------------------------------------------------------------------------------
-# Create annotation data for experiment DO-NO
-##------------------------------------------------------------------------------
-group_do_no <- factor(c("DF_Old", "DF_Old", 
-                        "N_Old", "N_Old"))
-samples_do_no <- c("DF_Old1", "DF_Old2",
-                   "N_Old1", "N_Old2")
-colData_do_no <- DataFrame(group_do_no, row.names= samples_do_no)
-
-# Read ENCODE data and store them in the required format
-wgbs_do_no <- readENCODEdata(bs_files[1:4], colData_do_no)
-
-# Find overlaps between promoter regions and WGBS data
-overlaps_do_no <- findOverlaps(prom_regions, rowRanges(wgbs_do_no))
-
-# Perform M3D statistic
-MMD_data_do_no <- M3D_Wrapper_lite(wgbs_do_no, overlaps_do_no)
-
-
-
 # ##------------------------------------------------------------------------------
-# # Create annotation data for experiment DY-NY
+# # Create annotation data for experiment DO-NO
 # ##------------------------------------------------------------------------------
-# group_dy_ny <- factor(c("DF_Young", "DF_Young", 
-#                         "N_Young", "N_Young"))
-# samples_dy_ny <- c("DF_Young1", "DF_Young2",
-#                    "N_Young1", "N_Young2")
-# colData_dy_ny <- DataFrame(group_dy_ny, row.names= samples_dy_ny)
+# group_do_no <- factor(c("DF_Old", "DF_Old", 
+#                         "N_Old", "N_Old"))
+# samples_do_no <- c("DF_Old1", "DF_Old2",
+#                    "N_Old1", "N_Old2")
+# colData_do_no <- DataFrame(group_do_no, row.names= samples_do_no)
 # 
 # # Read ENCODE data and store them in the required format
-# wgbs_dy_ny <- readENCODEdata(bs_files[5:8], colData_dy_ny)
+# wgbs_do_no <- readENCODEdata(bs_files[1:4], colData_do_no)
 # 
 # # Find overlaps between promoter regions and WGBS data
-# overlaps_dy_ny <- findOverlaps(prom_regions, rowRanges(wgbs_dy_ny))
+# overlaps_do_no <- findOverlaps(prom_regions, rowRanges(wgbs_do_no))
 # 
 # # Perform M3D statistic
-# MMD_data_dy_ny <- M3D_Wrapper_lite(wgbs_dy_ny, overlaps_dy_ny)
-# 
-# 
-# 
+# MMD_data_do_no <- M3D_Wrapper_lite(wgbs_do_no, overlaps_do_no)
+
+
+
+##------------------------------------------------------------------------------
+# Create annotation data for experiment DY-NY
+##------------------------------------------------------------------------------
+group_dy_ny <- factor(c("DF_Young", "DF_Young", 
+                        "N_Young", "N_Young"))
+samples_dy_ny <- c("DF_Young1", "DF_Young2",
+                   "N_Young1", "N_Young2")
+colData_dy_ny <- DataFrame(group_dy_ny, row.names= samples_dy_ny)
+
+# Read ENCODE data and store them in the required format
+wgbs_dy_ny <- readENCODEdata(bs_files[5:8], colData_dy_ny)
+
+# Find overlaps between promoter regions and WGBS data
+overlaps_dy_ny <- findOverlaps(prom_regions, rowRanges(wgbs_dy_ny))
+
+# Perform M3D statistic
+MMD_data_dy_ny <- M3D_Wrapper_lite(wgbs_dy_ny, overlaps_dy_ny)
+
+
+
 # ##------------------------------------------------------------------------------
 # # Create annotation data for experiment DY-DO
 # ##------------------------------------------------------------------------------
@@ -122,14 +122,14 @@ MMD_data_do_no <- M3D_Wrapper_lite(wgbs_do_no, overlaps_do_no)
 
 save(bs_files,
      prom_regions,
-     colData_do_no,
-     wgbs_do_no,
-     overlaps_do_no,
-     MMD_data_do_no, 
-#      colData_dy_ny,
-#      wgbs_dy_ny,
-#      overlaps_dy_ny,
-#      MMD_data_dy_ny,
+#      colData_do_no,
+#      wgbs_do_no,
+#      overlaps_do_no,
+#      MMD_data_do_no, 
+     colData_dy_ny,
+     wgbs_dy_ny,
+     overlaps_dy_ny,
+     MMD_data_dy_ny,
 #      colData_dy_do,
 #      wgbs_dy_do,
 #      overlaps_dy_do,
@@ -138,5 +138,5 @@ save(bs_files,
 #      wgbs_no_ny,
 #      overlaps_no_ny,
 #      MMD_data_no_ny,
-     file = paste0("../datasets/BEATSON/M3D_", downstream, "_DO_NO.RData"))
+     file = paste0("../datasets/BEATSON/M3D_", downstream, "_DY_NY.RData"))
 
